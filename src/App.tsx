ab64a039b227ba14772975eaba2879f0a2c15525
Sean Aguinaga <aguinaga.sean@gmail.com>
2018-11-21 10:09:45 -0800

    Master
    added basic dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '../src/configs/styled-components'
 
 class App extends Component {
+
   render() {
+    const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: red
+  `;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container>
+      Hello
+      </Container>
     );
   }
 }
